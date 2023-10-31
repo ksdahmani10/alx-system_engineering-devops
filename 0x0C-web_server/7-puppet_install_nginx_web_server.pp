fbd855f7825e730133dd51580d73ba2050f0481b
ksdahmani10 <ks.dahmani10@gmail.com>
2023-10-31 05:55:33 -0700

    adv

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# Install a web server Nginx
+# listening on port 80
+# GET root / return a page that contains the string Holberton School
+# The redirection is “301 Moved Permanently”
+
+package { 'nginx':
+ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+content => 'Holberton School',
+}
+
+file_line {'Add redirection, 301':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server',
+  line   => 'rewrite ^/redirect_me https://github.com/ permanent;',
+}
+
+service { 'nginx':
+ensure  => running,
+require => Package['nginx'],
+}
